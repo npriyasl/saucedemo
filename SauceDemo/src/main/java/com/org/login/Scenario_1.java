37992c34e8ea2211e35b2afe8acb4b0614f0f51d
Priya <Priya@LAPTOP-OE6B3LF3>
2022-05-04 22:44:20 +0530

    Scenario 1 : 1. Login into the site 2. Add the product to the cart 3. Checkout and validate the product details whether you are ordered the correct product
    Scenario 4:
    
    1. Enter the invalid username and password
    2. Validate the error message
    
    https://www.saucedemo.com/

---
+++ SauceDemo/src/main/java/com/org/login/Scenario_1.java
@@ -0,0 +1,48 @@
+package com.org.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Scenario_1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver","D:/Selenium/chromedriver_win32/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+	driver.get("https://www.saucedemo.com/");
+	
+	driver.findElement(By.id("user-name")).sendKeys("standard_user");
+	driver.findElement(By.id("password")).sendKeys("secret_sauce");
+	driver.findElement(By.id("login-button")).click();
+	
+	driver.findElement(By.xpath("//div[text()='Sauce Labs Backpack']")).click();
+	driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+	
+	WebElement prod= driver.findElement(By.xpath("//div[text()='Sauce Labs Backpack']"));
+	String prodvalue = prod.getText();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
+	
+	driver.findElement(By.id("checkout")).click();		
+		
+	driver.findElement(By.id("first-name")).sendKeys("Priya");
+	driver.findElement(By.id("last-name")).sendKeys("N");
+	driver.findElement(By.id("postal-code")).sendKeys("600001");
+	driver.findElement(By.id("continue")).click();
+	
+	Thread.sleep(3000);
+	WebElement prodcheck = driver.findElement(By.xpath("//div[text()='Sauce Labs Backpack']"));
+	
+	String checkvalue= prodcheck.getText();
+	
+	if (checkvalue.equals(prodvalue)) {
+		System.out.println("Product Ordered successfully");
+	}
+	System.out.println("success");
+	
+		
+	}
+}
---
+++ SauceDemo/src/main/java/com/org/login/Scenario_4.java
@@ -0,0 +1,70 @@
+package com.org.login;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.io.FileHandler;
+
+import com.google.common.io.Files;
+
+public class Scenario_4 {
+
+	public static void main (String[] args) throws IOException {
+		System.setProperty("webdriver.chrome.driver","D:/Selenium/chromedriver_win32/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.saucedemo.com/");
+		
+		TakesScreenshot sc = (TakesScreenshot)driver;
+		File srcFile = sc.getScreenshotAs(OutputType.FILE);
+		File to = new File("D:/Sel Practice/SauceDemo/Screenshot/login.png");
+		Files.copy(srcFile, to);
+		
+		System.out.println("Login page screenshot");
+		
+		WebElement Uname = driver.findElement(By.id("user-name"));
+		System.out.println("Username Enabled: " + Uname.isEnabled());
+		
+		WebElement Pwd = driver.findElement(By.id("password"));
+		System.out.println("Password Enabled: " + Pwd.isEnabled());
+		
+		WebElement Login = driver.findElement(By.id("login-button"));
+		System.out.println("Login Button Enabled: " + Login.isEnabled());
+		
+		Uname.sendKeys("standard_user");
+		Pwd.sendKeys("secret_saucea");
+		Login.click();
+		
+		String ErrMsg = "Epic sadface: Username and password do not match any user in this service";
+		
+		//WebElement error = driver.findElement(By.xpath("//button[@class='error-button']"));
+		WebElement error1 = driver.findElement(By.xpath("//h3[text()='Epic sadface: Username and password do not match any user in this service']"));
+		//System.out.println(error.getText());
+		System.out.println(error1.getText());
+		String a = error1.getText();
+		
+	    if (ErrMsg.matches(a)) {
+	    	System.out.println("Error Message is Displayed correctly");
+	    	File srcFile1 = sc.getScreenshotAs(OutputType.FILE);
+			File to1 = new File("D:/Sel Practice/SauceDemo/Screenshot/errormessage.png");
+			Files.copy(srcFile1, to1);
+	    		    }
+	    else 
+	    {
+	    	System.out.println("Incorrect error message displayed");
+	    	File srcFile1 = sc.getScreenshotAs(OutputType.FILE);
+			File to1 = new File("D:/Sel Practice/SauceDemo/Screenshot/errormessage.png");
+			Files.copy(srcFile1, to1);
+	    }
+	    	
+		
+	}
+}
